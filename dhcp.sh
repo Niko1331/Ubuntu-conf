a83aec09bcfd45d688c3fbde32c420307c02c601
NIko1331 <hogrider09231@gmail.com>
2026-03-31 18:25:10 +0200

    Nowa Funkcja - Dodano w dhcp.sh możliwość wpisania ręcznie adresu serwera DNS

--- dhcp.sh
+++ dhcp.sh
@@ -3,10 +3,11 @@
 set -e
 
 read -p "Podaj najpierw nazwę karty LAN żebym mógł zrobić zmiany w /etc/default/isc-dhcp-server: " LAN_IF
-read -p "Podaj swój adres karty LAN: " LAN_IP
+read -p "Podaj swój adres ip karty LAN: " LAN_IP
 read -p "Podaj swoją podsieć czyli adres karty LAN ale dopisz 0 na końcu: " NETWORK
 read -p "Podaj swój zakres początkowy serwera DHCP: " RANGE_START
 read -p "Podaj swój zakres końcowy serwera DHCP: " RANGE_END
+read -p "Podaj adres serwera DNS: " DNS_IP
 
 sudo bash -c "cat > /etc/default/isc-dhcp-server" << EOF
 INTERFACESv4="$LAN_IF"
@@ -22,7 +23,7 @@ max-lease-time 7200;
 subnet $NETWORK netmask 255.255.255.0 {
 	range $RANGE_START $RANGE_END;
 	option routers $LAN_IP;
-	option domain-name-servers 8.8.8.8;
+	option domain-name-servers $DNS_IP;
 }
 
 authoritative;
